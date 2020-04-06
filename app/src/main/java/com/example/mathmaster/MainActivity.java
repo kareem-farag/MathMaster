d39d2bd5697b283a07ca477b2f05f209dc4229af
Kareem Farag <kareemfarag@hotmail.com>
2020-04-06 16:29:30 +0200

    commit #1 : Building a random number method

--- app/src/main/java/com/example/mathmaster/MainActivity.java
+++ app/src/main/java/com/example/mathmaster/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.mathmaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("Random number :" , String.valueOf(generateRandomArray()[0]));
+        Log.i("Random number :" , String.valueOf(generateRandomArray()[1]));
+        Log.i("Random number :" , String.valueOf(generateRandomArray()[2]));
+        Log.i("Random number :" , String.valueOf(generateRandomArray()[3]));
+
     }
+    protected int[] generateRandomArray () {
+        int[] randomNumbers = new int[4] ;
+        Random random = new Random();
+        randomNumbers[0]= random.nextInt(21);
+        randomNumbers[1]= random.nextInt(21);
+        randomNumbers[2]= random.nextInt(21);
+        randomNumbers[3]= random.nextInt(21);
+
+        return randomNumbers ;
+    };
 }
