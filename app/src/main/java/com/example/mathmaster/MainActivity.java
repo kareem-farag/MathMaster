d8564af7a54d9e83dba3cecb699fef26e57f6b84
Kareem Farag <kareemfarag@hotmail.com>
2020-04-06 16:47:15 +0200

    commit #3 : Testing CountDownTimer Method

--- app/src/main/java/com/example/mathmaster/MainActivity.java
+++ app/src/main/java/com/example/mathmaster/MainActivity.java
@@ -4,22 +4,28 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView timerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        timerView = findViewById(R.id.timerView);
+        startTimer();
+    /*
         Log.i("Random number :" , String.valueOf(generateRandomArray()[0]));
         Log.i("Random number :" , String.valueOf(generateRandomArray()[1]));
         Log.i("Random number :" , String.valueOf(generateRandomArray()[2]));
         Log.i("Random number :" , String.valueOf(generateRandomArray()[3]));
-
+    */
     }
     protected int[] generateRandomArray () {
         int[] randomNumbers = new int[4] ;
@@ -37,13 +43,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTick(long millisUntilFinished) {
 
-
+                timerView.setText(String.valueOf(millisUntilFinished/1000));
             }
 
             @Override
             public void onFinish() {
+                timerView.setText("finished");
 
             }
-        };
+        }.start();
     }
 }
